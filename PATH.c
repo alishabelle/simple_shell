604e3c89aac2a43fba67d854da3fca312b6afa76
747-diego <725@holbertonschool.com>
2019-04-19 14:25:47 +0000

    Betty passes through

--- PATH.c
+++ PATH.c
@@ -8,30 +8,21 @@
 
 char **PATHfind(char **enviroment)
 {
-        char *find = "PATH=", **ptr;
-        unsigned int command, name;
+	char *find = "PATH=", **ptr;
+	unsigned int command, name;
 
-        for (command = 0; enviroment[command] != NULL; command++)
-        {
-                for (name = 0; name < 5; name++)
-                        if (find[name] != enviroment[command][name])
-                                break;
-                if (name == 5)
-                        break;
-        }
+	for (command = 0; enviroment[command] != NULL; command++)
+	{
+		for (name = 0; name < 5; name++)
+			if (find[name] != enviroment[command][name])
+				break;
+		if (name == 5)
+			break;
+	}
 
-	ptr = vect(&enviroment[command][5], _strlen(&enviroment[command][5]), \
-":");
+	ptr = vect(&enviroment[command][5], _strlen(&enviroment[command][5]), ":");
 
 	command = 0, name = 0;
 
-/*	for (; ptr[command] != NULL; command++)
-	{
-		for (; *ptr[name] != '\0'; name++)
-			;
-
-
-
-			}*/
-        return (ptr);
+	return (ptr);
 }
