28ad0e3878dd8fe2dc8fdb360cea000bddad8471
747-diego <725@holbertonschool.com>
2019-04-19 14:01:40 +0000

    Betty Approved

--- squirtle.c
+++ squirtle.c
@@ -2,6 +2,64 @@
 #include <stdio.h>
 
 /**
+ *exists - checking if the concatenated space exists
+ *@s1: first string passed in
+ *@s2: second string passed in
+ * Return: returns a pointer to a char
+ **/
+char *exists(char *s1, char **s2)
+{
+	int i = 0, chk;
+	char *ptr;
+
+	for (; s2[i] != NULL; i++)
+	{
+		ptr = _strncon(s2[i], s1);
+		chk = access(ptr, F_OK);
+		if (chk == 0)
+			return (ptr);
+		else if (chk == -1)
+			free(ptr);
+	}
+	return (NULL);
+}
+/**
+ *_strncon - creates space to put concatenated strings into
+ *@s1: first string passed
+ *@s2: second string passed
+ * Return: returns a pointer to a char
+ */
+char *_strncon(char *s1, char *s2)
+{
+	int lens1, lens2, x = 0, y = 0;
+	char *fullPath;
+
+	lens1 = _strlen(s1);
+	lens2 = _strlen(s2);
+
+	fullPath = malloc(sizeof(char) * (lens1 + lens2 + 2));
+
+	if (fullPath == NULL)
+		return (NULL);
+
+	for (; s1[x] != '\0'; x++)
+		fullPath[x] = s1[x];
+
+	fullPath[x] = '/';
+	x++;
+
+	for (; s2[y] != '\0'; y++, x++)
+		fullPath[x] = s2[y];
+
+	fullPath[x] = '\0';
+
+	return (fullPath);
+}
+
+
+
+
+/**
  *sighandler - function to end when ctrl D is used
  *@n: parameter
  * Return - returns a void
@@ -17,77 +75,49 @@ void sighandler(int n __attribute__((unused)))
  * main - Simple Shell
  * @argc: number of arguements
  * @argv: Arguments stored as string
- * Return: 0
+ *@envp: environment passed in
+* Return: 0
  */
-
 int main(int argc, char *argv[], char *envp[])
-
 {
-	char *buffer = NULL;
+	char *buffer = NULL, **cmd, **path, *fullPath;
 	size_t buffsize = 0;
 	pid_t newProcess;
 	ssize_t charCount;
 	int status/* , index = 0 */;
-	char **cmd, **path, *fullPath;
 
 	if (argc < 1)
 		return (-1);
-
-
 	signal(SIGINT, sighandler);
-
-
-
 	while (1)
-	{
-		write(STDERR_FILENO, "$ ", 2);
+	{	write(STDERR_FILENO, "$ ", 2);
 		charCount = getline(&buffer, &buffsize, stdin);
-
 		if (*buffer == '\n')
 			continue;
 		if (charCount < 1)
 			break;
-
 		if (buffer[charCount - 1] == '\n')
 			buffer[charCount - 1] = '\0';
-
 		SHELLexit(buffer);
-
-
-
-   /*exitShell(buffer;)*/
- /*array = */	cmd = vect(buffer, charCount, "\t\r\n ");
+		cmd = vect(buffer, charCount, "\t\r\n ");
 		path = PATHfind(envp);
 		fullPath = exists(cmd[0], path);
-   /* cd(array[0], array[1]); */
 		newProcess = fork();
-
 		if (newProcess < 0)
 			perror(argv[0]);
-
 		if (newProcess == 0)
-		{
-			execve(fullPath, cmd, envp);
+		{	execve(fullPath, cmd, envp);
 			perror(argv[0]);
 			freeArray(cmd);
 		}
 		else
-		  {
-			wait(&status);
+		{	wait(&status);
 			free(cmd[0]);
 			free(cmd);
-		  }
+		}
 	}
-
 	if (charCount < 0)
 		write(STDERR_FILENO, "\n", 1);
-
 	free(buffer);
- 	/*while (cmd[index])
-	{
-	  free(cmd[index]);
-		index++;
-		free(cmd);
-		}*/
 	return (0);
 }
